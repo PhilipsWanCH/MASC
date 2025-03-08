ee81f46f088de8c723d5fc12b29cf775d6fcbdad
wzl0329 <52845811+wzl0329@users.noreply.github.com>
2025-03-08 15:18:22 +0800

    Delete install.py

--- install.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-from huggingface_hub import hf_hub_download  # Load model directly
-
-hf_hub_download(repo_id="internlm/internlm-7b", filename="config.json", local_dir="download")
